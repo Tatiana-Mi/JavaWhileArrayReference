0c3a7caca043af84002de3ed17cc910f166aa070
Tatiana Mikhailova <mikhailova.t.a@gmail.com>
2022-05-31 14:02:42 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args){
+        int i = 40;
+        while (i <= 60 && i >=40) {
+            System.out.println(i);
+            i = i + 4;
+        }
+
+    int q = 40;
+    for (;q <= 60 && q >=40;){
+        System.out.println(q);
+        q=q +5;
+
+    }
+    int w = 5;
+    do{
+        System.out.println(w);
+        w = w +1;
+    }while (w <10);
+
+}
+}
